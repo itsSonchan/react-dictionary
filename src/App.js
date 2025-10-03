2be80b6828e52c0eb4ad5323e7032b78caa46ac9
TheSonchan <sonja.grund@icloud.com>
2025-10-03 15:33:28 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <Search />
+      <footer>
+        This page was created by{" "}
+        <a href="https://www.shecodes.io/graduates/156305-sonja-grund">Sonja</a>{" "}
+        and is open-sourced on{" "}
+        <a href="https://github.com/itsSonchan">Github.</a>
+      </footer>
     </div>
   );
 }
